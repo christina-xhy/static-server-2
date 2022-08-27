36b88cd0ae7c0337ef5b53fff17202b6a36deb2d
christina <christinayxh@163.com>
2022-08-28 01:37:58 +0800

    cookie

--- server.js
+++ server.js
@@ -23,7 +23,43 @@ var server = http.createServer(function (request, response) {
 
   console.log("有个傻子发请求过来啦！路径（带查询参数）为：" + pathWithQuery);
 
-  if (path === "/register" && method === "POST") {
+  if (path === "/sign_in" && method === "POST") {
+    response.setHeader("Content-Type", "text/html;charset=UTF-8");
+    const userArray = JSON.parse(fs.readFileSync("./db/users.json"));
+    console.log("line 29" + userArray);
+    const array = [];
+    request.on("data", (chunk) => {
+      array.push(chunk);
+    });
+    request.on("end", () => {
+      const string = Buffer.concat(array).toString();
+      const obj = JSON.parse(string); //name password
+      const user = userArray.find(
+        (user) => user.name === obj.name && user.password === obj.password
+      );
+      if (user === undefined) {
+        response.statusCode = 400;
+        response.setHeader("Content-Type", "text/json;charset=UTF-8");
+        response.end(`{"errorCode":4001}`); //errorcode 编码 ，自定义
+      } else {
+        response.statusCode = 200;
+        response.setHeader("Set-Cookie", "logined=1");
+        response.end();
+      }
+    });
+  } else if (path === "/home.html") {
+    const cookie = request.headers["cookie"];
+    if (cookie === "loginded===1") {
+      const homeHtml = fs.readFileSync("./public/home.html").toString();
+      const string = homeHtml.replace("{{loginStatus}}", "已登陆");
+      response.write(string);
+    } else {
+      const homeHtml = fs.readFileSync("./public/home.html").toString();
+      const string = homeHtml.replace("{{loginStatus}}", "未登陆");
+      response.write(string);
+    }
+    response.end("home");
+  } else if (path === "/register" && method === "POST") {
     response.setHeader("Content-Type", "text/html;charset=UTF-8");
     const userArray = JSON.parse(fs.readFileSync("./db/users.json"));
     console.log("line 29" + userArray);
